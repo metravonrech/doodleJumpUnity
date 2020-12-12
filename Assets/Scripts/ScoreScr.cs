dd91caf51cd3e153b276515366e8527fdb6d8a9d
metravonrech <artemcernov79@gmail.com>
2020-12-12 04:26:09 +0300

    first commit

---
+++ Assets/Scripts/ScoreScr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScr : MonoBehaviour
+{
+    public int Score;
+    public Text ScoreText;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        AddScore();
+    }
+
+    void AddScore()
+    {
+        Score += 1;
+        ScoreText.text = "Score: " + Score.ToString();
+    }
+
+}
